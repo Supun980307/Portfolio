1507d03b780095e2229dc2629b57b480e24dc744
Supun Madusankha <76279098+Supun980307@users.noreply.github.com>
2023-10-15 01:36:22 +0530

    Header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import Image from '../assets/supun.png';
+import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from 'framer-motion';
 
 const Banner = () => {
-  return <div className='section' id='home'>Banner</div>;
+  return (
+  <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+          <h1 className='text-[50px] font-bold leading-[0.8] lg:text-[100px]'>
+            SUPUN <span>MADUSANKHA</span>
+          </h1>
+        <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+          <span className='text-white mr-4'>I am a</span>
+          <TypeAnimation sequence={['Developer', 2000, 'Designer', 2000,]} speed={50} className='text-accent' wrapper='span' repeat={Infinity} />
+        </div>
+        <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
+        A motivated recent graduate from the Sabaragamuwa University of Sri Lanka with a strong passion for continuous learning and a hands-on
+        approach to problem-solving. Seeking an opportunity to apply my skills
+        and further expand my knowledge while embracing new technologies and
+        concepts.
+        </p>
+        <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+          <button className='btn btn-lg'>Contact me</button>
+          <a href='#' className='text-gradient btn-link'>
+            My Portfolio
+          </a>
+        </div>
+
+        <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+          <a href='#'>
+            <FaGithub />
+          </a>
+          <a href='#'>
+            <FaLinkedin />
+          </a>
+          <a href='#'>
+            <FaFacebook />
+          </a>
+        </div>
+      </div>
+    
+
+        <div className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[382px]'>
+          <img src={Image} alt='' />
+        </div>
+      </div>
+    </div>
+  </section>
+  );
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Logo from '../assets/supun-logo.png';
 
 const Header = () => {
-  return <div className='bg-blue-300'>Header</div>;
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        
+        <a href='#'>
+          <img src={Logo} alt='' className='text-[20px]'/>
+        </a>
+        
+        <button className='btn btn-sm'>Work with me</button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
